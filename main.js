15afb0b2fc67580bd4b1e3b74cd9a43aab67f449
aarushvadlamani <81377894+aarushvadlamani@users.noreply.github.com>
2021-11-14 10:11:36 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -107,3 +107,39 @@ if(keyPressed=='67'){
     console.log("c");
 }
 }
+function up(){
+    if(player_y>=0){
+        player_y=player_y - block_image_height;
+        console.log("block image height =" + block_image_height);
+        console.log("When up key is pressed, X ="+player_x+" ,Y ="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+} 
+function down(){
+    if(player_y<=500){
+        player_y=player_y + block_image_height;
+        console.log("block image height ="+block_image_height);
+        console.log("When down key is pressed, X="+player_x+" ,Y ="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function left(){
+    if(player_x>=0){
+        player_x=player_x - block_image_width;
+        console.log("block image width ="+block_image_width);
+        console.log("When left key is pressed, X = "+player_x+" ,Y ="+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+function right(){
+    if(player_x<=800){
+        player_x=player_x + block_image_width;
+        console.log("block image width = "+block_image_width);
+        console.log("When right key is pressed, X = "+player_x+" ,Y ="+player_x);
+        canvas.remove(player_object);
+         player_update();
+    }
+}
